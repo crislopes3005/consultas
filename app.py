914bf4c361d8756371d49bbe917f6ea9150b05b8
crislopes3005 <148553965+crislopes3005@users.noreply.github.com>
2026-04-06 17:21:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ df_pordia['Date'] = pd.to_datetime(df_pordia['Date'], dayfirst=True)
 
 # Descrição curta
 df_paragrafos['descricao_curta'] = df_paragrafos['descricao'].apply(
-    lambda x: x[:30] + "..." if isinstance(x, str) and len(x) > 30 else x
+    lambda x: x[:50] + "..." if isinstance(x, str) and len(x) > 50 else x
 )
 
 # Estados
